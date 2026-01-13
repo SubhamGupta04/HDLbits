70a3fe2c19c0c58c46b5c156be8b4508f8628f7e
Subham Gupta D <sd23031919@gmail.com>
2026-01-13 22:58:49 +0530

    Check
    Removed commented-out code and fixed formatting.

--- Modulehierarchy/Adder2.v
+++ Modulehierarchy/Adder2.v
@@ -2,7 +2,7 @@ module top_module (
     input [31:0] a,
     input [31:0] b,
     output [31:0] sum
-);//
+);
     wire cout;
     add16 a1(a[15:0],b[15:0],1'b0,sum[15:0],cout);
     add16 a2(a[31:16],b[31:16],cout,sum[31:16],1'b0);
@@ -13,7 +13,4 @@ module add1 ( input a, input b, input cin,   output sum, output cout );
     assign sum=a^b^cin;
     assign cout=(a&b)|(b&cin)|(cin&a);
     
-
-// Full adder module here
-
 endmodule
